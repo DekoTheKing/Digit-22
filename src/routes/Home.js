db718a8c031c67a9e3791b4c8dd4bb3ed703d32e
Iva Milosavljevic <ivamilosavljevic@live.com>
2022-12-13 17:16:24 +0100

    Added cards on homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Video from '../components/Video'
+import CoffeeProducts from '../components/CoffeeProducts'
 
 const Home = () => {
   return (
     <div>
       <Video/>
+      <CoffeeProducts/>
+
     </div>
   )
 }
